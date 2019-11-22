3ba9cd5bcb46dfb6af4d330ad6ea528371d5fdc4
zs <zs@bjpowernode.com>
2019-11-22 17:16:33 +0800

    commit bus

---
+++ springcloud-bus/src/test/java/kan/haichuan/springcloudbus/SpringcloudBusApplicationTests.java
@@ -0,0 +1,13 @@
+package kan.haichuan.springcloudbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
